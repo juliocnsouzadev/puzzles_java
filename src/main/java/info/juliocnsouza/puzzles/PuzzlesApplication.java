a76afd0342f118c41eada09fbd3b5755350b3ee2
juliocnsouzadev <juliocnsouzadev@gmail.com>
2021-10-07 07:44:28 -0300

    init

---
+++ src/main/java/info/juliocnsouza/puzzles/PuzzlesApplication.java
@@ -0,0 +1,13 @@
+package info.juliocnsouza.puzzles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/juliocnsouza/puzzles/PuzzlesApplicationTests.java
@@ -0,0 +1,13 @@
+package info.juliocnsouza.puzzles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
