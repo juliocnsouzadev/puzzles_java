bd9e4d72b21d5660b0006def8372470c01bb22e8
juliocnsouzadev <juliocnsouzadev@gmail.com>
2021-10-24 13:39:14 -0300

    add js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,11 @@
-const salesByMatch = require("./salesByMatch.js");
+//const salesByMatch = require("./salesByMatch.js");
+//salesByMatch();
 
-salesByMatch();
+const sorted_squared = require("./sorted_squared.js");
+console.log(
+    JSON.stringify(
+        sorted_squared([-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        null,
+        2
+    )
+);
